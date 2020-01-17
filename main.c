6a787f3e40f805a2c2f52610a02044cb1c297da5
zerbem <59746097+zerbem@users.noreply.github.com>
2020-01-17 16:28:51 -0500

    1/17/2020 EGR 226-905_Zerbe_Smith_Lab2

--- main.c
+++ main.c
@@ -1,202 +1,170 @@
 /***********************************************
 * Names:        Marshall Zerbe, Michael Smith
 * Course:       EGR226-905
-* Project:      Lab 01, Coin Game
-* File:         DiceGame.c
-* Description:  This program is an autonomous gambling machine that takes credit input from the user
-                    and bet input from the user and then "rolls" a 15 sided dice to decide the winner.
+* Project:      Lab 02, Structured programming
+* File:         EGR226905_Zerbe_Smith_Lab2
+* Description:  This program reads in a file of book information from a txt file.
+                    The system then allows a user to search for books based on the book's
+                    author, ISBN, or title. The system stores all information in a typedef structure format.
 ************************************************/
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <time.h>
 
-int greetings();
-int creditCheck(int input);
-int bets(int maxBet);
-int betCheck(int credits, int maxBet);
-int rollDice();
-char compareRolls(int user, int system);
 
+
+typedef struct{
+char title[256];
+char author_name[51];
+char ISBN[11];
+int pages;
+int year_pub;
+}book_struct;
+
+void parse_file(book_struct book_array[]);
+void print_book(book_struct book_array[],int i);
+void search_title(book_struct book_array[], char Stitle[]);
+void search_ISBN(book_struct book_array[], char SISBN[]);
+void search_author(book_struct book_array[], char author[]);
 
 int main()
 {
-    srand(time(NULL));
 
-    int gameCredits, bettingCredits, userDice, systemDice;
-    char winFlag, contFlag='Y';
-
-    gameCredits=greetings();
+    char author[51], Stitle[256], SISBN[11], c;
+    int inFlag=0, choice, contFlag=0;
+    book_struct book_array[360];
+    parse_file(book_array);
 
     do{
-    bettingCredits=bets(gameCredits);
-
-    userDice=rollDice();
-    printf("You rolled a: %d\n", userDice);
-    printf("Rolling dice for system...\n");
-    systemDice=rollDice();
-    printf("System rolled a: %d\n", systemDice);
-
-    winFlag=compareRolls(userDice,systemDice);
-    if(winFlag=='u'){
-        printf("YOU WON!!\n");
-        gameCredits=gameCredits+bettingCredits;
-        printf("Your new total is: %d\n", gameCredits);
+    printf("How to do you want to search for books?\n[0] Search by title\n[1] Search by Author\n[2] Search by ISBN\n");
+    printf("Enter 3 to exit.");
+    while(scanf("%d", &choice) !=1){
+        c=getchar();
+        printf("You have entered ");
+        putchar(c);
+        printf(". This is not an option.");
+        while(getchar()!='\n');
+    };
+    if(choice>=0 && choice<=3){
+        inFlag=1;
     }
-    else if(winFlag=='s'){
-        printf("You Lose.");
-        gameCredits=gameCredits-bettingCredits;
-        printf("Your new total is: %d\n", gameCredits);
+    }while(inFlag==0);
+
+    do{
+    if(choice==0){
+        printf("Enter the title: ");
+        gets(Stitle);
+        printf("\n");
+        search_title(book_array, Stitle);
     }
-    else if(winFlag=='t'){
-        printf("Tie, returning your bet. Your total is: %d\n", gameCredits);
+    if(choice==1){
+        printf("Enter the Author's name: ");
+        gets(author);
+        printf("\n");
+        search_author(book_array, author);
+    }
+    if(choice==2){
+        printf("Enter the ISBN: ");
+        gets(SISBN);
+        printf("\n");
+        search_ISBN(book_array, SISBN);
+    }
+    if(choice==3){
+        contFlag=1;
     }
 
-    if(gameCredits<=0){
-        printf("Game Over\n\n");
-        break;
-    }
-    printf("Do you wish to continue (Y/N)?");
-    fflush(stdin);
-    scanf("%c", &contFlag);
-    if(contFlag!='Y' && contFlag!='N'){
-        printf("Requires CAPITAL Y or N.\n");
-        printf("Do you wish to continue (Y/N)?\n");
-        scanf("%c", &contFlag);
-    }
+    }while(contFlag==0);
 
-
-    }while(contFlag=='Y');
-    printf("\nFinal balance: %d\nGoodBye", gameCredits);
     return 0;
 }
 
-int greetings()
-/************************************
-*Brief:     greets player and asks for their starting credits
-
-*Params:    int inCredits:  the starting game credits between 0-500
-            int creditFlag: Used to ensure a proper input has been received
-            char c:         used to ensure no letter was entered
-
-*Returns    int inCredits:  used in other functions for calculating proper bets and new totals
-************************************/
+void parse_file(book_struct book_array[])
 {
-    int inCredits, creditFlag=0;
-    char c;
-    printf("Welcome to the dice game!!\n\n");
-    do{
-    printf("Please enter your credits. Minumum of 0 to play and a max of 500 at once.\n");
-    while(scanf("%d", &inCredits) !=1){
-        c=getchar();
-        printf("You have entered ");
-        putchar(c);
-        printf(" as your credits, this is not an integer. Enter a whole number between 0 and 500 to play\n");
-        while(getchar()!='\n');
-    };
+    FILE *fpin;
+    char buffer[512];
+    int i=0;
 
-    printf("You entered: %d\n", inCredits);
-    creditFlag=creditCheck(inCredits);
-    }while(creditFlag==0);
-    printf("Credits accepted. Starting game.\n\n");
-    return inCredits;
+    if((fpin=fopen("books.csv","r"))==NULL){
+        printf("File DNE.");
+        exit(1);
+    }
+
+    while(fgets(buffer, 512, fpin)){
+        char *ptr = strtok(buffer,",");
+        if(strcmp(ptr,"N/A")){
+            strcpy(book_array[i].title,ptr);
+        }
+        ptr=strtok(NULL,",\n");
+        if(strcmp(ptr,"N/A")){
+            strcpy(book_array[i].author_name,ptr);
+        }
+        ptr=strtok(NULL,",\n");
+        if(strcmp(ptr,"N/A")){
+            strcpy(book_array[i].ISBN,ptr);
+        }
+        ptr=strtok(NULL,",\n");
+        if(strcmp(ptr,"N/A")){
+            book_array[i].pages=ptr;
+        }
+        ptr=strtok(NULL,",\n");
+        if(strcmp(ptr,"N/A")){
+            book_array[i].year_pub=ptr;
+        }
+        i++;
+    }
+    fclose(fpin);
+}
+
+void print_book(book_struct book_array[], int i)
+{
+
+    printf("Title: ");
+    puts(book_array[i].title);
+    printf("\n");
+
+    printf("Author: ");
+    puts(book_array[i].author_name);
+    printf("\n");
+
+    printf("ISBN: ");
+    puts(book_array[i].ISBN);
+    printf("\n");
+
+    printf("# of Pages: %d\n", book_array[i].pages);
+
+    printf("Year published: %d\n\n", book_array[i].year_pub);
+
 
 }
 
-int creditCheck(int input)
-/************************************
-*Brief:     Checks the inCredits for validity
-
-*Params:    int input:      inCredits variable being checked
-            int creditFlag: changed based on validity of input
-
-*Returns:   int creditFlag: allows greetings() to continue, 1, or restart, 0
-************************************/
+void search_title(book_struct book_array[], char Stitle[])
 {
-    int creditFlag=0;
-    if(input<=500 && input>=0){
-        creditFlag=1;
+    int i=0;
+    while(i<=360){
+        if(strstr(book_array[i].title,Stitle)!=0){
+            print_book(book_array,i);
+        }
+        i++;
     }
-    else if(input>500 || input<0){
-        creditFlag=0;
-    }
-
-    return creditFlag;
 }
-
-int bets(int maxBet)
-/************************************
-*Brief:     Asks the user to place their bet and then checks the bet's validity with betCheck()
-
-*Params:    int betFlag:    changes based on validity of bet
-            int betCredits: saves the user's bet for later
-            char c:         used to insure no letter was placed as a bet
-
-*Returns:   int betCredits: Used later for calculating new balance
-************************************/
+void search_ISBN(book_struct book_array[], char SISBN[])
 {
-    int betFlag=0, betCredits;
-    char c;
-    do{
-    printf("Place your bet. Do not exceed your available credits.\n");
-    while(scanf("%d", &betCredits) !=1){
-        c=getchar();
-        printf("You have entered ");
-        putchar(c);
-        printf(" as your credits, this is not an integer. Enter a whole number between 0 and 500 to play\n");
-        while(getchar()!='\n');
-    };
-    printf("You bet: %d\n", betCredits);
-    betFlag=betCheck(betCredits, maxBet);
-    }while(betFlag==0);
-    printf("\nrolling D-15 for user's number.\n");
-    return betCredits;
+    int i=0;
+    while(i<=360){
+        if(strstr(book_array[i].ISBN,SISBN)!=0){
+            print_book(book_array,i);
+        }
+        i++;
+    }
 }
-
-int betCheck(int betcredits, int maxBet)
-/************************************
-*Brief: Checks the bet's validity based on game credits and 0
-
-*Params:    int betcredits: The user's entered bet to be validated from bets()
-            int maxBet:     the gameCredits entered into greetings(). Used to ensure
-                                the bet does not surpass current playable credits
-            int betFlag:    used to return if a bet is valid
-
-*Returns:   int betFlag:    returned to bets() to continue or restart
-************************************/
+void search_author(book_struct book_array[], char author[])
 {
-    int betFlag=0;
-    if(betcredits>maxBet || betcredits<0){
-        betFlag=0;
+    int i=0;
+    while(i<=360){
+        if(strstr(book_array[i].author_name,author)!=0){
+            print_book(book_array,i);
+        }
+        i++;
     }
-    else if(betcredits<=maxBet && betcredits>=0){
-        betFlag=1;
-    }
-    return betFlag;
 }
-
-int rollDice()
-{
-    int dice;
-    do{
-        dice=rand();
-    }while(dice<1 || dice>15);
-    return dice;
-}
-
-char compareRolls(int user, int system)
-{
-    char winFlag='s';
-    if(user>system){
-        winFlag='u';
-    }
-    else if(user<system){
-        winFlag='s';
-    }
-    else if(user==system){
-        winFlag='t';
-    }
-    return winFlag;
-}
-
