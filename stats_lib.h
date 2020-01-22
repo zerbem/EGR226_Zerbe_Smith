2b059eb788a7a03f3cdbf8beb29d2646e27c6c06
smithmr3 <59878708+smithmr3@users.noreply.github.com>
2020-01-21 20:08:33 -0500

    Zerbe_Smith_Lab1_stats_library

---
+++ stats_lib.h
@@ -0,0 +1,15 @@
+/***********************************************
+* Names:        Marshall Zerbe, Michael Smith
+* Course:       EGR226-905
+* Project:      Lab 01, Stats Library
+* File:         stats_lib.h
+* Description:  This program defines the functions for the library
+************************************************/
+
+int maximum(float nums[], int n);                   //maximum function
+int minimum (float nums[], int n);                  //minimum function
+double mean(float nums[], int n);                   //mean function
+float median(float nums[], int n);                  //median function
+float variance(float nums[], int n);                //variance function
+double standard_deviation(float nums[], int n);     //standard_deviation
+
