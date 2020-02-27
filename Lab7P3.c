2d0c5229e3603ba5fbc07c208deda2324f538c55
smithmr3 <59878708+smithmr3@users.noreply.github.com>
2020-02-27 11:53:45 -0500

    Update Lab7P3.c

--- Lab7P3.c
+++ Lab7P3.c
@@ -11,7 +11,7 @@
 
 #define RS 1         //4.0 mask
 #define EN 4         //4.2 mask
-char line1[15];     //character string for the printout
+char line1[16];     //character string for the printout
 
 void systick_init();
 void systick_delay(uint16_t millsec);
@@ -33,18 +33,19 @@ void main(void)
     line1[0] = 'L';
     line1[1] = 'A';
     line1[2] = 'B';
-    line1[3] = 'R';
-    line1[4] = 'A';
-    line1[5] = 'T';
-    line1[6] = 'O';
-    line1[7] = 'R';
-    line1[8] = 'Y';
-    line1[9] = ' ';
-    line1[10] ='O';
-    line1[11] ='V';
-    line1[12] ='E';
-    line1[13] ='R';
-    line1[14] =' ';
+    line1[3] = 'O';
+    line1[4] = 'R';
+    line1[5] = 'A';
+    line1[6] = 'T';
+    line1[7] = 'O';
+    line1[8] = 'R';
+    line1[9] = 'Y';
+    line1[10] = ' ';
+    line1[11] ='O';
+    line1[12] ='V';
+    line1[13] ='E';
+    line1[14] ='R';
+    line1[15] =' ';
 
     commandWrite(1);
     systick_delay(500);
@@ -56,13 +57,13 @@ void main(void)
         int n = 1,j;                            //initializing int for counting
         commandWrite(0x80);                     //start at position 00
         commandWrite(0x06);
-        for(i=0;i<15;i++)                       //Print "Labratory over to screen hold for 1 second
+        for(i=0;i<16;i++)                       //Print "Labratory over to screen hold for 1 second
         {
             dataWrite(line1[i]);
         }
         systick_delay(1000);
 
-        for(j=0;j<14;j++){                      //Reprint but skip letter n until all are gone
+        for(j=0;j<15;j++){                      //Reprint but skip letter n until all are gone
             commandWrite(0x01);                 //Reprint at home location
             systick_delay(50);                  //delay to get home
             for(i=n;i<14;i++){                  //begin at the nth letter
@@ -74,7 +75,7 @@ void main(void)
 
         commandWrite(0x06);                     //increment printout
         commandWrite(0x8F);                     //Start from line 1 right
-        for(i=0;i<15;i++){                      //print line1[]
+        for(i=0;i<16;i++){                      //print line1[]
             dataWrite(line1[i]);                //print the current ith letter
             systick_delay(1000);                //wait 1 second
             commandWrite(0x18);                 //shift display 1 to the left
